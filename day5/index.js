9ecf5b5b355deec166850a440cde2d0edfafeb4d
VladOnet21 <onet.vlad@yahoo.com>
2023-01-16 20:26:33 +0200

    day 5 -Proprietati si Metode.

---
+++ day5/index.js
@@ -0,0 +1,13 @@
+console.log('Teaching the world how to code'.length); // Afisam numarul de caractere al sirului
+
+console.log('Codecademy'.toUpperCase()); // Transformam toate literele in litere mari
+
+console.log('    Remove whitespace   '.trim());// Eliminam spatiile din fata si de la finalul stringului
+
+console.log(Math.random() * 100); // Generam un random si il inmultim
+
+console.log(Math.floor(Math.random() * 100)); // Generam un numar intreg
+
+console.log(Math.ceil(43.8)); // returnam cem mai mic integer mai mic sau egal cu un numar decimal
+
+console.log(Number.isInteger(2017.1)); // verificam daca numarul intrrodus este integer
