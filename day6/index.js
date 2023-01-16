350edaacb14402710d0e56e4e2f7178575a2033b
VladOnet21 <onet.vlad@yahoo.com>
2023-01-16 21:25:10 +0200

    day6- Var,Let,Const.

---
+++ day6/index.js
@@ -0,0 +1,18 @@
+// Var declaration 
+var favoriteFood = 'pizza'; // asignarea unui string intr o variabila
+var numOfSlices = 8; // asignarea unui integer intr o variabila
+
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+// Let declation
+let changeMe = true;
+changeMe = false;
+
+console.log(changeMe);
+
+//Const declaration
+const entree ='Enchiladas';
+
+console.log(entree);
+entree = 'Tacos';
