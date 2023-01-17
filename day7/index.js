88f2460c73cc0f32e8255b1074cff1f960b2c323
VladOnet21 <onet.vlad@yahoo.com>
2023-01-17 11:31:19 +0200

    day7 -Mathematical Assignment Operators

---
+++ day7/index.js
@@ -0,0 +1,14 @@
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
