144532210c3c864f93ce236f47358c0850d4ca0a
VladOnet21 <onet.vlad@yahoo.com>
2023-01-17 11:38:30 +0200

    day7 -Mathematical Assignment Operators

---
+++ day7/increment_decrement.js
@@ -0,0 +1,11 @@
+let gainedDollar = 3;
+let lostDollar = 50;
+
+console.log(gainedDollar);
+console.log(lostDollar);
+
+gainedDollar++;
+lostDollar--;
+
+console.log(gainedDollar);
+console.log(lostDollar);
