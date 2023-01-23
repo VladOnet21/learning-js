ec08ba5e9e546ea862d673bfd509defca1a73047
VladOnet21 <onet.vlad@yahoo.com>
2023-01-23 12:22:19 +0200

    day 11: return

--- day11/parameters.js
+++ day11/parameters.js
@@ -1,6 +1,6 @@
 function sayThanks(name) {
   console.log(
-    "Thank you for your purchase " + name + "! We appreciate your business."
+    'Thank you for your purchase ' + name + '! We appreciate your business.'
   );
 }
-sayThanks("Cole");
+sayThanks('Cole');
