3284f9815abfa1f241e978746832839d2c17164a
VladOnet21 <onet.vlad@yahoo.com>
2023-01-26 19:43:37 +0200

    day 11 functions

--- day11/function_expresion.js
+++ day11/function_expresion.js
@@ -1,7 +1,2 @@
-const plantNeedsWater = function (day) {
-    if (day === 'Wednesday') {
-      return true;
-    } else return false;
-  };
-  console.log(plantNeedsWater('Tuesday'));
-  
+const plantNeedsWater = (day) => day === 'Wednesday'; 
+console.log(plantNeedsWater('Tuesday'));
