3e842a54bef4ac7364ecfa77caacd9554c3a4c79
VladOnet21 <onet.vlad@yahoo.com>
2023-01-26 20:53:06 +0200

    day 14 array method

--- day14/array_function.js
+++ day14/array_function.js
@@ -1,7 +1,7 @@
-const concept = ["arrays", "can", "be", "mutated"];
+const concept = ['arrays', 'can', 'be', 'mutated'];
 
 function changeArr(arr) {
-  arr[3] = "MUTATED";
+  arr[3] = 'MUTATED';
 }
 
 changeArr(concept);
