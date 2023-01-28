15b2a3360995bc7c234a46183faac54c9200f4fa
VladOnet21 <onet.vlad@yahoo.com>
2023-01-28 14:14:44 +0200

    day15 looping arrays

---
+++ day15/while_loop.js
@@ -0,0 +1,6 @@
+const cards = ['diamond', 'spade', 'heart', 'club'];
+let currentCard;
+while (currentCard !== 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
