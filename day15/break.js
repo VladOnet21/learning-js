d3b12a93cb3f3c14943732575b4b683d7dcef345
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-01-29 14:50:42 +0200

    day15: clean-up over using single quotes.

--- day15/break.js
+++ day15/break.js
@@ -1,11 +1,9 @@
-const rapperArray = ["Lil' Kim", 'Jay-Z', 'Notorious B.I.G.', 'Tupac'];
+const rapperArray = ['Lil\' Kim', 'Jay-Z', 'Notorious B.I.G.', 'Tupac'];
 
-for (let i=0;i<rapperArray.length;i++){
+for (let i = 0; i < rapperArray.length; i++) {
   console.log(rapperArray[i]);
-  if(rapperArray[i]==='Notorious B.I.G.'){
+  if (rapperArray[i] === 'Notorious B.I.G.') {
     break;
   }
 }
-console.log("And if you don't know, now you know.");
-
-//s-au folosit apostroafe duble deoarce numele muzicienilor contineau apostroafe simple si sa nu se creeze confuzie
+console.log('And if you don\'t know, now you know.');
