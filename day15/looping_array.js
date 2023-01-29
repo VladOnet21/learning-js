248365e482e53e0807e919fdfb13686e48e6bf83
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-01-29 15:02:15 +0200

    day15: clean-up over iterating over arrays and added multiple ways of iteration.

--- day15/looping_array.js
+++ day15/looping_array.js
@@ -1,4 +1,20 @@
 const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+/**
+ * for loop
+ */
 for (let i = 0; i < vacationSpots.length; i++) {
-  console.log('I would love to visit '+ vacationSpots[i]);
+  console.log(`I would love to visit ${vacationSpots[i]} FOR`);
+}
+
+/**
+ * forEach loop
+ */
+vacationSpots.forEach(vacationSpot => console.log(`I would love to visit ${vacationSpot} FOREACH`));
+
+/**
+ * for...of loop
+ */
+for (const vacationSpot of vacationSpots) {
+  console.log(`I would love to visit ${vacationSpot} FOR OF`);
 }
--- day15/while_loop.js
+++ day15/while_loop.js
@@ -1,6 +1,7 @@
 const cards = ['diamond', 'spade', 'heart', 'club'];
 let currentCard;
 while (currentCard !== 'spade') {
-  currentCard = cards[Math.floor(Math.random() * 4)];
+  const generateRandomNumber = Math.floor(Math.random() * 4);
+  currentCard = cards[generateRandomNumber];
   console.log(currentCard);
 }
