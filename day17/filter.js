8398606f91208533ccc858afbe19876adfe50f17
VladOnet21 <onet.vlad@yahoo.com>
2023-01-30 19:50:52 +0200

    day17 foreach method an other

---
+++ day17/filter.js
@@ -0,0 +1,15 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+const smallNumbers = randomNumbers.filter((number) => number < 250);
+console.log(smallNumbers);
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+
+const longFavoriteWords = favoriteWords.filter((word) => word.length > 7);
+console.log(longFavoriteWords);
