fba3ed2664144a844ef4f1ad5a69df05fe2beab5
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-02-02 21:54:57 +0200

    day17: added proper indentation and remove unused code.

--- day17/right_iterator.js
+++ day17/right_iterator.js
@@ -1,28 +1,23 @@
 const cities = [
-    "Orlando",
-    "Dubai",
-    "Edinburgh",
-    "Chennai",
-    "Accra",
-    "Denver",
-    "Eskisehir",
-    "Medellin",
-    "Yokohama",
-  ];
-  
-  const nums = [1, 50, 75, 200, 350, 525, 1000];
-  
-  cities.forEach((city) => console.log("Have you visited " + city + "?"));
-  
-  const longCities = cities.filter((city) => city.length > 7);
-  
-  const word = cities.reduce((acc, currVal) => {
-    return acc + currVal[0];
-  }, "C");
-  
-  console.log(word);
-  
-  const smallerNums = nums.map((num) => num - 5);
-  
-  nums.some((num) => num < 0);
-  
+  'Orlando',
+  'Dubai',
+  'Edinburgh',
+  'Chennai',
+  'Accra',
+  'Denver',
+  'Eskisehir',
+  'Medellin',
+  'Yokohama',
+];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0];
+}, 'C');
+
+console.log(word);
+
+nums.some(num => num < 0);
