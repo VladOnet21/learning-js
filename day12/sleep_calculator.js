23f6c2199de2c8b486640e0a7cb4225fb638e6ea
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-02-04 23:36:13 +0200

    sweet refactoring for the sleep calculator app.

--- day12/sleep_calculator.js
+++ day12/sleep_calculator.js
@@ -1,51 +1,37 @@
 const getSleepHours = day => {
-  const getDay = day.toLowerCase();
-  switch (getDay) {
-    case 'monday':
-      return 8;
-    case 'tuesday':
-      return 8;
-    case 'wednesday':
-      return 6;
-    case 'thursday':
-      return 7;
-    case 'friday':
-      return 5;
-    case 'saturday':
-      return 10;
-    case 'sunday':
-      return 11;
-    default:
-      return 'Error';
-  }
-};
-const getActualSleepHours = () => {
-  const sum = getSleepHours('Monday')
-    + getSleepHours('Tuesday')
-    + getSleepHours('Wednesday')
-    + getSleepHours('Thursday')
-    + getSleepHours('Friday')
-    + getSleepHours('Saturday')
-    + getSleepHours('Sunday');
-  return sum;
+  const dayMapper = {
+    monday: 8,
+    tuesday: 8,
+    wednesday: 6,
+    thursday: 7,
+    friday: 5,
+    saturday: 10,
+    sunday: 11,
+  };
+
+  return dayMapper[day.toLowerCase()];
 };
 
-const getIdealSleepHours = () => {
-  const idealHours = 8;
-  return idealHours * 7;
+const getActualSleepHours = () => {
+  const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+  return days.reduce((acc, curr) => acc + getSleepHours(curr), 0);
 };
+
+const getIdealSleepHours = () => 8 * 7;
 
 const calculateSleepDebt = () => {
   const actualSleepHours = getActualSleepHours();
   const idealSleepHours = getIdealSleepHours();
+
   if (actualSleepHours === idealSleepHours) {
     console.log('Perfect amount of sleep.');
   } else if (actualSleepHours > idealSleepHours) {
-    const dif = actualSleepHours - idealSleepHours;
-    console.log(`Got more sleep than needed, with ${dif} hours`);
+    const difference = actualSleepHours - idealSleepHours;
+    console.log(`Got more sleep than needed, with ${difference} hours`);
   } else {
-    const dif1 = idealSleepHours - actualSleepHours;
-    console.log(`Should get some rest ${dif1} hours`);
+    const difference = idealSleepHours - actualSleepHours;
+    console.log(`You should get some rest, with ${difference} hours`);
   }
 };
 
