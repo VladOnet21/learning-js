e396940ba625fead98064fc3bcc7e8adc3b0cbc7
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-02-04 22:20:54 +0200

    day19: replaced let with const.

--- day19/accesing_props.js
+++ day19/accesing_props.js
@@ -1,11 +1,13 @@
-let spaceship = {
+const spaceship = {
   homePlanet: 'Earth',
   color: 'silver',
   'Fuel Type': 'Turbo Fuel',
   numCrew: 5,
   flightPath: ['Venus', 'Mars', 'Saturn'],
 };
+
 const crewCount = spaceship.numCrew;
 const planetArray = spaceship.flightPath;
-console.log(crewCount);
-console.log(planetArray);
+
+console.log(crewCount, 'crew members');
+console.log(planetArray, 'planets');
