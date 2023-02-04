62347353e1f3ce2be9a94154d3fc3a250c693b7e
VladOnet21 <onet.vlad@yahoo.com>
2023-02-04 13:45:13 +0200

    day19 Objects

---
+++ day19/bracket_notation.js
@@ -0,0 +1,11 @@
+let spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    'Active Mission': true,
+    homePlanet: 'Earth',
+    numCrew: 5,
+  };
+  
+  const isActive = spaceship['Active Mission'];
+  console.log(isActive);
+  console.log(spaceship['Active Mission']);
+  
