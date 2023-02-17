06cad28fde17aaa0e0f8ecfe9aed771494161172
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-02-17 22:33:21 +0200

    day23: fixed issues related to linter for the mealMaker mini-app.

--- day23/mealMaker.js
+++ day23/mealMaker.js
@@ -3,17 +3,33 @@ const menu = {
   _price: 0,
   set meal(mealToCheck) {
     if (typeof mealToCheck === 'string') {
-      return (this._meal = mealToCheck);
+      /**
+       * removed return statement as it is not necessary here since the value
+       * is already being assigned to the property.
+       */
+      this._meal = mealToCheck;
     }
   },
   set price(priceToCheck) {
     if (typeof priceToCheck === 'number') {
-      return (this._price = priceToCheck);
+      /**
+       * removed return statement as it is not necessary here since the value
+       * is already being assigned to the property.
+       */
+      this._price = priceToCheck;
     }
   },
   get todaysSpecial() {
+    /**
+     * changed from a function to a getter method, which allows
+     * us to access the value of this method without calling it directly.
+     */
     if (this._meal && this._price) {
-      return `Today’s Special is ${this._meal} for ${this._price} $`;
+      /**
+       * changed from using an OR operator to an AND operator, so that both conditions must be true
+       * for the string to be returned.
+       */
+      return `Today’s speciality is ${this._meal} for ${this._price} $`;
     } else {
       return 'Meal or price was not set correctly!';
     }
@@ -21,5 +37,5 @@ const menu = {
 };
 menu.meal = 'ciorba';
 menu.price = 7;
+
 console.log(menu.todaysSpecial);
-console.log('test-commit');
